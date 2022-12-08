34bcfe3e4f0e8eda728feb03f49ff8a7a93ed6db
munguriek <benjaminmunguriek@gmail.com>
2022-12-08 07:39:12 +0300

    Added the pages and base template

--- pages/urls.py
+++ pages/urls.py
@@ -5,4 +5,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('', views.about, name='about'),
+    path('', views.cars, name='cars'),
+    path('', views.services, name='services'),
+    path('', views.contact, name='contact'),
 ]
