17b03a2eabe687a1e6456fab61fa31c594a51eb3
munguriek <benjaminmunguriek@gmail.com>
2024-03-11 20:28:27 +0300

    Completed the initial setup of the Django and HTML skeleton styled page

--- config/settings.py
+++ config/settings.py
@@ -118,7 +118,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+# Define your static files directory
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+# Optional: Define the directory where collectstatic will put your static files for deployment
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
