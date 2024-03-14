89c7363e35e386d4eaf17653fa9379f359241526
munguriek <benjaminmunguriek@gmail.com>
2024-03-14 19:51:14 +0300

    Added the Teams model

--- config/settings.py
+++ config/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'wdb_impex',
+        'USER': 'wsu_impex',
+        'PASSWORD': 'Awesome@2021',
+        'HOST': 'localhost',
+        'PORT': 5432
     }
 }
 
